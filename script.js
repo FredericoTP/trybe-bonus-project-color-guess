9e4edf411737c4240d0e38bfab55aff1ea8460d6
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-18 10:08:34 -0300

    mudança

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const scoreNumber = document.getElementById('scoreNumber');
 function randomColor() {
   const red = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 101);
+  const green = Math.floor(Math.random() * 256);
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
